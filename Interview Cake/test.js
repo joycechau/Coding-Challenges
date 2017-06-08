1d6d61b8d494f0d9259d7dc69adf550ebedbcfa8
Joyce Chau <jyc.chau@gmail.com>
2017-06-07 23:47:12 -0700

    Remove test functions

--- Interview Cake/test.js
+++ Interview Cake/test.js
@@ -1,19 +1,1 @@
-function varTest() {
-  var x = 1;
-  if (true) {
-    var x = 2;  // same variable!
-    console.log(x);  // 2
-  }
-  console.log(x);  // 2
-}
 
-function letTest() {
-  let x = 1;
-  if (true) {
-    let y = 2;  // different variable
-    console.log(x);  // 2
-  }
-  console.log(y);  // 1
-}
-
-letTest()
